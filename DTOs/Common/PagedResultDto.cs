6ef1eddaf66585dee916a49020103888471b8dc4
Sujeepan <psujee07@gmailcom>
2026-08-07 11:05:22 +0530

    Add shared infrastructure components

---
+++ DTOs/Common/PagedResultDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Recruitment_Project.DTOs.Common
+{
+    public class PagedResultDto
+    {
+    }
+}
---
+++ Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Recruitment_Project.Helpers
+{
+    public class ClaimsPrincipalExtensions
+    {
+    }
+}
