ea0633639eaaf74ebe438a1376bfe5fb12684efe
Sujeepan <psujee07@gmailcom>
2026-08-07 10:52:10 +0530

    Add shared infrastructure components

---
+++ Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+
+namespace Recruitment_Project.Helpers
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            var claim = user.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim == null)
+                throw new UnauthorizedAccessException("User ID claim not found.");
+
+            return int.Parse(claim.Value);
+        }
+
+        public static string GetUserRole(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Role)?.Value ?? string.Empty;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Email)?.Value ?? string.Empty;
+        }
+    }
+}
